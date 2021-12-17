a01822292328f54aa4fd4fc6621c6e6f26a68d64
Kirill Strukov <kirillstrukov2@gmail.com>
2021-12-17 17:58:30 +0300

    задание не доделано!!!

---
+++ HW6.js
@@ -0,0 +1,58 @@
+// Window
+// Дмитрий
+// undefined
+// undefined
+// Дмитрий
+// undefined
+// undefined undefined
+// undefined
+// undefined
+// Владимир
+// Hello, World! каждую секунду
+// logMessage.call(object);
+// undefined
+// const calculator = {
+//   setValues(a, b) {
+//     this.a = a;
+//     this.b = b;
+//     if (typeof (a, b) !== "number") {
+//       console.log("Ошибка1");
+//     } else return this.a, this.b;
+//   },
+//   sum() {
+//     if (this.a && this.b) {
+//       return this.a + this.b;
+//     } else {
+//       console.log("Ошибка2");
+//     }
+//   },
+//   mult() {
+//     if (this.a && this.b) {
+//       return this.a * this.b;
+//     } else {
+//       console.log("Ошибка3");
+//     }
+//   },
+// };
+// calculator.setValues(100, 20);
+// console.log(calculator.sum(), calculator.mult(), calculator);
+// const calculator2 = Object.assign({}, calculator.setValues);
+// // const calculator2 = Object.assign({}, calculator);
+// // const calculator2 = {
+// //   setValues: Object.assign({}, calculator.setValues(1,2)),
+// //     div() {
+// //       if (this.a && this.b) {
+// //         return this.a / this.b;
+// //       } else {
+// //         console.log("Ошибка4");
+// //       }
+// //     },
+// //     diff() {
+// //       if (this.a && this.b) {
+// //         return this.a - this.b;
+// //       } else {
+// //         console.log("Ошибка5");
+// //       }
+// //     },
+// // };
+// console.log(calculator2);
